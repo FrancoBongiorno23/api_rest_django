6bceee8ffc081c7b52c82737032f1e57b9886d00
Nestor <tanonester@hotmail.com>
2022-04-19 10:37:11 -0300

    Segundo commit, el test utomatizado funciona y devuelve OK

--- Bongiorno_API_REST/mysite/myapi/tests.py
+++ Bongiorno_API_REST/mysite/myapi/tests.py
@@ -3,8 +3,8 @@ import json
 from django.contrib.auth.models import User
 from django.urls import reverse
 # esta era la anterior linea from profiles.api.serializers import ProfileSerializer
-from .serializers import ProfileSerializer
-from profiles.models import Profile
+# esta linea deberia ir  from rest_framework import ProfileSerializer
+# dejo este en comentario from rest_framework import Profile
 from rest_framework import status
 
 from rest_framework.authtoken.models import Token
@@ -13,7 +13,9 @@ from rest_framework.test import APITestCase
 class RegistrationTestCase(APITestCase):
 
     def test_registration(self):
-        data = {"username": "testcase", "email": "correo@testing.com" ,
-                "password1": "passwordstrong-", "password2": "passwordstrong-2"}
-        response=self.client.post("/api-auth/login/", data)
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        data = {"nombre": "ejemplo", "apellido": "ejemploapellido" ,
+                "correo": "correo@testing.com"}
+        response=self.client.post("/lectores/", data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED);
+
+#el error radicaba en el endpoint donde me confundi el endpoint de /lectores/ con /libros/
