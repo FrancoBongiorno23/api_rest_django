efb051b84d72b72aee08c21c0c44e87bfd8c6f9b
Nestor <tanonester@hotmail.com>
2022-06-06 23:12:26 -0300

    Se eliminan los modelos de tablas no usadas y sus referencias en cada uno de los archivos correspondientes

--- myapi/models.py
+++ myapi/models.py
@@ -1,40 +1,11 @@
 from django.db import models
 #Pongo nombres de modelos en singular ej. LIbro
 
-class Autor(models.Model):
-    #id=models.AutoField(primary_key = True)
-    apellido=models.CharField(max_length = 120)
-    nombre=models.CharField(max_length = 100)
-    email=models.EmailField(max_length=200)
-
-    def __str__(self):
-        return self.apellido
-
-
-class Editorial(models.Model):
-    id=models.AutoField(primary_key = True)
-    nombre=models.CharField(max_length = 120)
-
-    def __str__(self):
-        return self.nombre
-
-
-class Categoria(models.Model):
-    id=models.AutoField(primary_key = True)
-    nombre=models.CharField(max_length = 120)
-
-    def __str__(self):
-        return self.nombre
-
-
 class Libro(models.Model):
     id=models.AutoField(primary_key = True)
     isbn=models.CharField(max_length=20)
     titulo=models.CharField(max_length = 100)
     fechaLanzamiento=models.DateField()
-    #idAutor= models.ForeignKey(Autor, on_delete=models.CASCADE)
-    idCategoria=models.ForeignKey(Categoria, on_delete=models.CASCADE)
-    idEditorial= models.ForeignKey(Editorial, on_delete=models.CASCADE)
     idioma=models.CharField(max_length = 100)
     paginas=models.IntegerField()
     descripcion=models.TextField(max_length = 400)
@@ -53,10 +24,3 @@ class Lector(models.Model):
     def __str__(self):
         return self.nombre
 
-
-class Alquiler(models.Model):
-    id=models.AutoField(primary_key = True)
-    idLector=models.ForeignKey(Lector, on_delete=models.CASCADE)
-    idLibro=models.ForeignKey(Libro, on_delete=models.CASCADE)
-    fechaSalida=models.DateField()
-    fechaentrada=models.DateField()
